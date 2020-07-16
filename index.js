b4a9e4a296dce4e32e19fb6c2357f4ed96ed83e7
Quincey Hollman <qhollman@gmail.com>
2020-07-16 13:44:09 -0700

    clearing tree for new branch

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-require('dotenv').config();
-const Discord = require('discord.js');
+require("dotenv").config();
+const Discord = require("discord.js");
 const bot = new Discord.Client();
 const TOKEN = process.env.BOT_TOKEN;
 
-bot.once('ready', () => {
+bot.once("ready", () => {
   console.log(`Hello, I am ${bot.user.tag}!`);
 });
 
-bot.on('message', (message) => {
-  if (message.content.includes('hello')) {
+bot.on("message", (message) => {
+  if (message.content.includes("hello")) {
     message.reply(`Hello I am ${bot.user.tag}!\n`);
-    message.channel.send('How can I help you?');
+    message.channel.send("How can I help you?");
   }
 });
 
