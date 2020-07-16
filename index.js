a3d70b88195a2848bead29d8aa90baf426e2972e
Quincey Hollman <qhollman@gmail.com>
2020-07-16 14:05:19 -0700

    response to word jasmine

--- index.js
+++ index.js
@@ -11,11 +11,13 @@ bot.on("message", (message) => {
   if (message.content.includes("hello")) {
     message.reply(`Hello I am ${bot.user.tag}!\n`);
     message.channel.send("How can I help you?");
-  } else if (message.content.includes("jasmine")) {
-    message.reply(`Yes! Let's talk about Jasmine testing!`);
   }
 });
 
-bot.on("message", (message) => {});
+bot.on("message", (message) => {
+  if (message.content.includes("jasmine")) {
+    message.channel.send(`Yes! Let's talk about Jasmine testing!`);
+  }
+});
 
 bot.login(TOKEN);
