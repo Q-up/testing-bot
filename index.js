28ed72fa328235204cd7c61f5fc85a7041ab1fc4
ArielRenée <rios94.a@gmail.com>
2020-07-16 20:46:55 -0700

    Added more message commands | edited ESLint rules

--- index.js
+++ index.js
@@ -1,21 +1,29 @@
-require("dotenv").config();
-const Discord = require("discord.js");
+require('dotenv').config();
+const Discord = require('discord.js');
 const bot = new Discord.Client();
 const TOKEN = process.env.BOT_TOKEN;
 
-bot.once("ready", () => {
+bot.once('ready', () => {
   console.log(`Hello, I am ${bot.user.tag}!`);
 });
 
-bot.on("message", (message) => {
-  if (message.content.includes("hello")) {
-    message.reply(`Hello I am ${bot.user.tag}!\n`);
-    message.channel.send("How can I help you?");
-  } else if (message.content.includes("jasmine")) {
-    message.channel.send(`Yes! Let's talk about Jasmine testing!\n`);
-    message.channel.send(
-      `To see Jasmine's official documentation follow this link: https://jasmine.github.io/`
-    );
+bot.on('message', (message) => {
+  const msg = message.content.toLowerCase();
+
+  if (msg.includes('hello')) {
+    message.reply(`What's poppin I am ${bot.user.tag}!\n`);
+    message.channel.send('How can I help you?');
+  } else if (msg.includes('jasmine')) {
+     message.channel.send('Yes! Let\'s talk about testing!\n What programming langauge are you looking for?');
+  } else if (msg.includes('javascript')) {
+    message.channel.send('iJS (im just sayin) this might help: https://bit.ly/398eWFE');
+    message.reply('Does that help?');
+  } else if (msg.includes('python')) {
+    message.channel.send('I love py! Thisss might help: https://bit.ly/3fFd4Xd');
+    message.reply('Does that help?');
+  } else if (msg === 'ruby') {
+    message.channel.send('You said ruby? Okay, here\'s a gem for you: https://bit.ly/3h8FaKG');
+    message.reply('Does that help?');
   }
 });
 
